8df6417c8d9a9e7b9123ad47d7145febbe553395
Dima Dzundza <dzundzan@gmail.com>
2022-11-12 13:45:32 +0200

    merged carousel infto main

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__, template_folder="./resources/html")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+class Cat(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Cat %r>' % self.id
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+
+    birthday = db.Column(db.DateTime)
+
+    gender = db.Column(db.Integer) # TODO: сделать проверку, что 0 или 1
+    content = db.Column(db.String(200), nullable=False)
+
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
 
 @app.route("/")
 def index():
