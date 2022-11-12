c3fc239fae678042f1b11d63162ca344a4a76324
Dima Dzundza <dzundzan@gmail.com>
2022-11-12 12:15:52 +0200

    added footer. fixed navbar

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="./resources/html")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
