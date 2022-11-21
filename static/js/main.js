b2791372dae7836565164253288bc646398c5f7c
Khomenko Dmytro <hds2014228@gmail.com>
2022-11-21 21:03:26 +0200

    Edit log and reg forms.

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,42 @@
-
 $("#login_section").hide();
 $("#register_section").hide();
 $(".form-conteiner").hide();
 $(".form-close-bg").hide();
+$(".exit-reg-bg").hide();
+$(".exit-login-bg").hide();
 
-$("#navbar_login_button").on( "click", function( event ) {
+$(".exit-reg-bg").on( "click", function( event ) {
     $("#register_section").hide();
-    $("#login_section").show();
+    $(".exit-reg-bg").hide();
+});
+
+$(".exit-login-bg").on( "click", function( event ) {
+    $("#login_section").hide();
+    $(".exit-login-bg").hide();
+});
+
+$(".reg-close-icon").on( "click", function( event ) {
+    $("#register_section").hide();
+    $(".exit-reg-bg").hide();
+});
+$(".log-close-icon").on( "click", function( event ) {
+    $("#login_section").hide();
+    $(".exit-login-bg").hide();
 });
 $("#navbar_register_button").on( "click", function( event ) {
+    $(".exit-login-bg").hide();
     $("#login_section").hide();
     $("#register_section").show();
+    $(".exit-reg-bg").show();
 });
+
+$("#navbar_login_button").on( "click", function( event ) {
+    $(".exit-reg-bg").hide();
+    $("#register_section").hide();
+    $("#login_section").show();
+    $(".exit-login-bg").show();
+});
+
 $(".btn-form-photo").on( "click", function( event ) {
     $(".form-conteiner").show();
     $(".form-close-bg").show();
